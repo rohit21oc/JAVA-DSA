cacebd85ec59e4408996c0e3aff8a8426ca63fb9
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2024-10-14 23:42:25 +0530

    Add files via upload

---
+++ function/factorial.java
@@ -0,0 +1,15 @@
+package function;
+
+
+public class factorial {
+    public static int factOfNum(int n){
+        int f =1;
+        for(int i=1;i<=n;i++){
+            f = f * i;
+        }
+        return f; //factorial of n
+    }
+    public static void main(String[] args) {
+        System.out.println(factOfNum(4));
+    }
+}
